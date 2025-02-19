9ea350490cad02b7002dd0e3359a72068e089c82
VikasSangwan21 <Vaticidevikk@gmail.com>
2025-02-19 12:25:22 +0800

    updated test data

--- testData/testData.js
+++ testData/testData.js
@@ -7,14 +7,11 @@ module.exports = {
     validUser6: { birthYear: "2000", income: "Between $22,001 and $34,000", multipleProperty: "Yes", housing: "3-room flat",propertyOwnership: "Rented from Open market"  },
     validUser7: { birthYear: "1985", income: "Between $34,001 and $100,000", multipleProperty: "Yes", housing: "4-room flat",propertyOwnership: "Rented from Open market"  },
     validUser8: { birthYear: "2000", income: "Above $100,000", multipleProperty: "Yes", housing: "3-room flat",propertyOwnership: "Rented from Open market"  },
-    validUser9: { birthYear: "1985", income: "30000", multipleProperty: "Yes", housing: "4-room flat",propertyOwnership: "Rented from Open market"  },
-    validUser10: { birthYear: "2000", income: "15000", multipleProperty: "Yes", housing: "3-room flat",propertyOwnership: "Rented from Open market"  },
     belowAgeLimit: { birthYear: "2005", income: "", multipleProperty: "No", housing: "4-room flat",propertyOwnership: "Rented from Open market" },
-    invalidUser: { birthYear: "abxc", income: "-5000", multipleProperty: "", housing: "",propertyOwnership: ""  },
-    missingYOB: { birthYear: "", income: "$22,000 and below or No income", multipleProperty: "No", housing: "4-room flat",propertyOwnership: "Rented from Open market" },
+    missingYOB: { birthYear: "", income: "Between $34,001 and $100,000", multipleProperty: "No", housing: "4-room flat",propertyOwnership: "Rented from Open market" },
     missingAI: { birthYear: "1999", income: "", multipleProperty: "No", housing: "4-room flat",propertyOwnership: "Rented from Open market" },
-    missingPropertyOwnership: { birthYear: "1999", income: "$22,000 and below or No income", multipleProperty: "No", housing: "",propertyOwnership: "Rented from Open market" },
-    missingHousing: { birthYear: "1999", income: "$22,000 and below or No income", multipleProperty: "No", housing: "4-room flat",propertyOwnership: "" },
-    missingMultipleOwnership: { birthYear: "1999", income: "$22,000 and below or No income", multipleProperty: "", housing: "4-room flat",propertyOwnership: "Rented from Open market" },
-    familyMember1: { birthYear: "1986", income: "$22,000 and below or No income", multipleProperty: "No", housing: "4-room flat",propertyOwnership: "Rented from Open market" }
+    missingPropertyOwnership: { birthYear: "1999", income: "$Between $34,001 and $100,000", multipleProperty: "No", housing: "",propertyOwnership: "Rented from Open market" },
+    missingHousing: { birthYear: "1999", income: "Between $34,001 and $100,000", multipleProperty: "No", housing: "4-room flat",propertyOwnership: "" },
+    missingMultipleOwnership: { birthYear: "1999", income: "Between $34,001 and $100,000", multipleProperty: "", housing: "4-room flat",propertyOwnership: "Rented from Open market" },
+    familyMember1: { birthYear: "1986", income: "Between $34,001 and $100,000", multipleProperty: "No", housing: "4-room flat",propertyOwnership: "Rented from Open market" }
   };
